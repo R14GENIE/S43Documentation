cfa676bf869d28131b2987f07ecd547c6421c5ac
R14 <69074781+R14GENIE@users.noreply.github.com>
2024-01-18 07:48:11 +0100

    Creation du script example

---
+++ ScriptExemple.lua
@@ -0,0 +1,63 @@
+--[[
+    Service
+]]
+
+--DECLARATION DE SERVICE
+
+--[[
+    Require
+]]
+
+--DECLARATION DES REQUIRES
+
+--[[
+    TweenInfo
+]]
+
+--DECLARATION DES TWEENINFO
+
+--[[
+    CONSTANTE
+]]
+
+--DECLARATION DES CONSTANTE
+
+--[[
+    Value
+]]
+
+--DECLARATION DE VALEUR
+
+--[[
+    Remote Event / Remote Function
+]]
+
+--DECLARATION DES REMOTE EVENTS
+
+--[[
+    Object Value
+]]
+
+--DECLARATION DE VALEUR OBJECT / REFERENCE A DES INSTANCES
+
+--[[
+    TABLE
+]]
+
+--[[
+    CASE
+]]
+
+--DECLARATION DES TABLE CASE
+
+--[[
+    FUNCTION
+]]
+
+--VOS FUNCTIONS
+
+--[[
+    MODULE
+]]
+
+--VOS MODULE
